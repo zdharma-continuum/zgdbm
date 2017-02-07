4603ff8a084d8b9a3569452269b89434d47279f6
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2017-02-07 10:06:09 +0100

    Put module to "psprint" namespace

--- zgdbm.plugin.zsh
+++ zgdbm.plugin.zsh
@@ -25,7 +25,7 @@ fi
 # Compile the module
 #
 
-if [ ! -e "${ZGDBM_REPO_DIR}/module/Src/Modules/zgdbm.so" ]; then
+if [ ! -e "${ZGDBM_REPO_DIR}/module/Src/psprint/zgdbm.so" ]; then
     builtin print "----------------------------"
     builtin print "${fg_bold[magenta]}psprint${reset_color}/${fg_bold[yellow]}zgdbm${reset_color} is building..."
     builtin print "----------------------------"
@@ -71,7 +71,7 @@ elif [[ ! -f "${ZGDBM_REPO_DIR}/module/COMPILED_AT" || ( "${ZGDBM_REPO_DIR}/modu
 fi
 
 # Finally load the module - if it has compiled
-if [[ -e "${ZGDBM_REPO_DIR}/module/Src/Modules/zgdbm.so" ]]; then
-    MODULE_PATH="${ZGDBM_REPO_DIR}/module/Src/Modules":"$MODULE_PATH"
-    zmodload zgdbm
+if [[ -e "${ZGDBM_REPO_DIR}/module/Src/psprint/zgdbm.so" ]]; then
+    MODULE_PATH="${ZGDBM_REPO_DIR}/module/Src":"$MODULE_PATH"
+    zmodload psprint/zgdbm
 fi
